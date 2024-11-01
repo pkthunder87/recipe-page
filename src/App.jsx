7949645afb3b4e1953516a9ecf88555a9c557455
pkthunder87 <pkthunder87@gmail.com>
2024-10-31 20:42:01 -0700

    Feat: Add attribution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <div className="main-title">Recipe Page</div>;
+  return (
+    <div>
+      <div>Recipe Page</div>
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
 }
 
 export default App;
