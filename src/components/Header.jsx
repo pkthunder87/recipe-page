8ae3f1fa1a036d28bc022e79c88627cdbc0b6036
pkthunder87 <pkthunder87@gmail.com>
2024-11-06 21:22:15 -0800

    Feat: Add Header and color scheme

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <section className="header">
+      <img
+        className="header__img"
+        src="../src/assets/images/image-omelette.jpeg"
+        alt="a tasty omelette"
+      />
+      <h1 className="header__title">Simple Omelette Recipe</h1>
+      <p className="header__text">
+        An easy and quick dish, perfect for any meal. This classic omelette
+        combines beaten eggs cooked to perfection, optionally filled with your
+        choice of cheese, vegetables, or meats.
+      </p>
+    </section>
+  );
+}
+
+export default Header;
