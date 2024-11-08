5b454e33f36d6026c257cb5aa0824f12b5562fc5
pkthunder87 <pkthunder87@gmail.com>
2024-11-07 23:49:56 -0800

    Feat: Add Prep component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import AppLayout from "./components/AppLayout";
 import Attribution from "./components/Attribution";
 import Header from "./components/Header";
+import Prep from "./components/Prep";
 import "./sass/main.scss";
 
 function App() {
   return (
     <AppLayout>
       <Header />
+      <Prep />
       <Attribution />
     </AppLayout>
   );
---
+++ src/components/Prep.jsx
@@ -0,0 +1,20 @@
+function Prep() {
+  return (
+    <section className="prep">
+      <div className="prep__title">Preparation time</div>
+      <ul className="prep__list">
+        <li>
+          <span className="prep__bold">Total: </span>Approximately 10 minutes
+        </li>
+        <li>
+          <span className="prep__bold">Preparation: </span>5 minutes
+        </li>
+        <li>
+          <span className="prep__bold">Cooking </span>5 minutes
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Prep;
